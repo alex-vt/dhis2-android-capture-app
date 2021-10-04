e323f8ef4ef59a60e9e5213ebbf127fb1dbaca98
Jorge Sánchez <xurxosf@gmail.com>
2021-10-04 10:09:09 +0200

    Modify presenter to search using the SearchPresenter

--- app/src/main/java/org/dhis2/usescases/biometrics/duplicates/BiometricsDuplicatesDialogPresenter.kt
+++ app/src/main/java/org/dhis2/usescases/biometrics/duplicates/BiometricsDuplicatesDialogPresenter.kt
@@ -1,40 +1,113 @@
 package org.dhis2.usescases.biometrics.duplicates
 
+import io.reactivex.Flowable
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.Consumer
+import org.dhis2.data.schedulers.SchedulerProvider
+import org.dhis2.data.search.SearchParametersModel
+import org.dhis2.usescases.searchTrackEntity.SearchRepository
+import org.dhis2.utils.NetworkUtils
+import org.hisp.dhis.android.core.D2
+import timber.log.Timber
 
-class BiometricsDuplicatesDialogPresenter() {
-
+class BiometricsDuplicatesDialogPresenter(
+    private val d2: D2,
+    private val searchRepository: SearchRepository,
+    private val schedulerProvider: SchedulerProvider
+) {
     lateinit var view: BiometricsDuplicatesDialogView
-    lateinit var guids: List<String>
-    lateinit var sessionId: String
+    lateinit var biometricsGuids: List<String>
+    lateinit var biometricsSessionId: String
+    lateinit var programUid: String
+    lateinit var trackedEntityTypeUid: String
+    lateinit var biometricsAttributeUid: String
 
     var disposable: CompositeDisposable = CompositeDisposable()
 
-    fun init(view: BiometricsDuplicatesDialogView, guids: List<String>, sessionId: String) {
+    fun init(
+        view: BiometricsDuplicatesDialogView,
+        biometricsGuids: List<String>,
+        biometricsSessionId: String,
+        programUid: String,
+        trackedEntityTypeUid: String,
+        biometricsAttributeUid: String
+    ) {
         this.view = view
-        this.guids = guids
-        this.sessionId = sessionId
+        this.biometricsGuids = biometricsGuids
+        this.biometricsSessionId = biometricsSessionId
+        this.programUid = programUid
+        this.trackedEntityTypeUid = trackedEntityTypeUid
+        this.biometricsAttributeUid = biometricsAttributeUid
 
         loadData()
     }
 
     private fun loadData() {
-        view.setData(guids.map { BiometricsDuplicatesDialogItem(it) })
-    }
+        val program = d2.programModule().programs().uid(programUid).blockingGet()
 
-    private fun getCategoryOptionCombos() {
-/*        disposable.add(
-            d2.categoryModule().categoryCombos().uid(uid).get()
+        disposable.add(
+            Flowable.just(
+                searchRepository.searchTrackedEntities(
+                    SearchParametersModel(
+                        program,
+                        trackedEntityTypeUid,
+                        hashMapOf(biometricsAttributeUid to biometricsGuids.joinToString(separator = ";"))
+                    ),
+                    NetworkUtils.isOnline(view.getContext())
+                )
+            ).doOnError { Timber.e(it) }
                 .subscribeOn(schedulerProvider.io())
                 .observeOn(schedulerProvider.ui())
                 .subscribe(
-                    { view.setTitle(it.displayName() ?: "-") },
-                    { Timber.e(it) }
-                )
-        )*/
+                    { liveData ->
+                        view.setLiveData(liveData)
+                    },
+                    { Timber.e(it) })
+        )
     }
 
     fun onDetach() {
         disposable.clear()
+    }
+
+    fun onTEIClick(teiUid: String, enrollmentUid: String, isOnline: Boolean) {
+        if (!isOnline) {
+            openDashboard(teiUid, enrollmentUid)
+        } else {
+            downloadTei(teiUid, enrollmentUid)
+        }
+    }
+
+    private fun openDashboard(teiUid: String, enrollmentUid: String) {
+        view.openDashboard(
+            teiUid,
+            programUid,
+            enrollmentUid
+        )
+    }
+
+    private fun downloadTei(teiUid: String?, enrollmentUid: String?) {
+        disposable.add(
+            searchRepository.downloadTei(teiUid)
+                .subscribeOn(schedulerProvider.io())
+                .observeOn(schedulerProvider.ui())
+                .subscribe(
+                    { view.downloadProgress() }, { Timber.d(it) },
+                    {
+                        if (d2.trackedEntityModule().trackedEntityInstances().uid(teiUid)
+                                .blockingExists()
+                        ) {
+                            openDashboard(teiUid!!, enrollmentUid!!)
+                        } else {
+                            val trackedEntityType = d2.trackedEntityModule().trackedEntityTypes()
+                                .uid(trackedEntityTypeUid).blockingGet()
+                            view.couldNotDownload(trackedEntityType.displayName()!!)
+                        }
+                    })
+        )
+    }
+
+    fun enrollNewClick() {
+        view.enrollNew(biometricsSessionId)
     }
 }
--- app/src/main/java/org/dhis2/usescases/biometrics/duplicates/BiometricsDuplicatesDialogView.kt
+++ app/src/main/java/org/dhis2/usescases/biometrics/duplicates/BiometricsDuplicatesDialogView.kt
@@ -1,5 +1,17 @@
 package org.dhis2.usescases.biometrics.duplicates
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.paging.PagedList
+import io.reactivex.functions.Consumer
+import org.dhis2.usescases.searchTrackEntity.adapters.SearchTeiModel
+import org.hisp.dhis.android.core.arch.call.D2Progress
+
 interface BiometricsDuplicatesDialogView {
-    fun setData(data: List<BiometricsDuplicatesDialogItem>)
+    fun setLiveData(liveData: LiveData<PagedList<SearchTeiModel>>)
+    fun getContext(): Context?
+    fun openDashboard(teiUid: String, programUid: String, enrollmentUid: String)
+    fun downloadProgress(): Consumer<D2Progress>
+    fun couldNotDownload(displayName: String)
+    fun enrollNew(biometricsSessionId: String)
 }
