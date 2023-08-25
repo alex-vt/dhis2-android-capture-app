7204192fca84f263bf9b7340b44083db012bfccb
xurxodev <xurxosf@gmail.com>
2023-08-25 11:54:49 +0200

    Remove non-simprints and dhis2 flavors

--- app/src/psi/java/org/dhis2/data/user/UserComponentFlavor.kt
+++
@@ -1,8 +0,0 @@
-package org.dhis2.data.user
-
-import org.dhis2.usescases.teiDashboard.dashboardsfragments.feedback.FeedbackComponent
-import org.dhis2.usescases.teiDashboard.dashboardsfragments.feedback.FeedbackModule
-
-interface UserComponentFlavor {
-    fun plus(feedbackModule: FeedbackModule): FeedbackComponent
- }
--- app/src/widp/java/org/dhis2/data/user/UserComponentFlavor.kt
+++
@@ -1,3 +0,0 @@
-package org.dhis2.data.user
-
-interface UserComponentFlavor {}
