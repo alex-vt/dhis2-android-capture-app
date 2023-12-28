2ba36e70e126a104850faf6ccbbb768f980d2170
xurxodev <xurxosf@gmail.com>
2023-12-28 15:21:31 +0100

    Extract logic to retrieve BiometricParentChildConfig from preferences

--- app/src/main/java/org/dhis2/data/biometrics/BiometricsParentChildConfigRepositoryImpl.kt
+++ app/src/main/java/org/dhis2/data/biometrics/BiometricsParentChildConfigRepositoryImpl.kt
@@ -1,10 +1,8 @@
 package org.dhis2.data.biometrics
 
-import com.google.gson.reflect.TypeToken
 import org.dhis2.commons.biometrics.BiometricsPreference
 import org.dhis2.commons.prefs.BasicPreferenceProvider
 import org.dhis2.usescases.biometrics.entities.BiometricsParentChildConfig
-import org.dhis2.usescases.biometrics.entities.DateOfBirthAttributeByProgram
 import org.dhis2.usescases.biometrics.repositories.BiometricsParentChildConfigRepository
 import timber.log.Timber
 
@@ -45,25 +43,6 @@ class BiometricsParentChildConfigRepositoryImpl(
     }
 
     override fun get(): BiometricsParentChildConfig {
-        val parentChildRelationship =
-            preferenceProvider.getString(BiometricsPreference.PARENT_CHILD_RELATIONSHIP, "")
-
-        val ageThreshold = preferenceProvider.getInt(BiometricsPreference.AGE_THRESHOLD, 0)
-
-        val sateOfBirthAttributeByProgramType =
-            object : TypeToken<List<DateOfBirthAttributeByProgram>>() {}
-
-        val dateOfBirthAttributeByProgram = preferenceProvider.getObjectFromJson(
-            BiometricsPreference.DATE_OF_BIRTH_ATT_BY_PROGRAM,
-            sateOfBirthAttributeByProgramType,
-            listOf()
-        )
-
-        return BiometricsParentChildConfig(
-            ageThreshold,
-            parentChildRelationship!!,
-            dateOfBirthAttributeByProgram
-        )
+        return getBiometricsParentChildConfig(preferenceProvider);
     }
-
 }
