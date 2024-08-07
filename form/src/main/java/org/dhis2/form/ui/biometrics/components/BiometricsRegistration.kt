886b0583e5fd30256d29de60daa8edd4d90b7679
xurxodev <xurxosf@gmail.com>
2024-08-07 08:14:58 +0200

    Reuse RegistrationButton in BiometricsRegistration

--- form/src/main/java/org/dhis2/form/ui/biometrics/components/BiometricsRegistration.kt
+++ form/src/main/java/org/dhis2/form/ui/biometrics/components/BiometricsRegistration.kt
@@ -1,126 +1,33 @@
 package org.dhis2.form.ui.biometrics.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import org.dhis2.commons.biometrics.BIOMETRICS_FAILURE_PATTERN
-import org.dhis2.commons.biometrics.getBioIconNew
-import org.dhis2.commons.biometrics.getBioIconSuccess
-import org.dhis2.commons.biometrics.getBioIconWarning
-import org.dhis2.commons.resources.ResourceManager
-import org.dhis2.form.R
-import org.hisp.dhis.mobile.ui.designsystem.component.Button
-import org.hisp.dhis.mobile.ui.designsystem.component.ButtonStyle
+import org.dhis2.form.ui.biometrics.components.biometricsTEIRegistration.BiometricsTEIState
+import org.dhis2.form.ui.biometrics.components.biometricsTEIRegistration.RegistrationButton
 
-enum class BiometricsState {
-    INITIAL,
-    SUCCESS,
-    FAILURE,
-}
 
 @Composable
 internal fun BiometricsRegistration(
     value: String?,
     onBiometricsClick: () -> Unit,
-    resources: ResourceManager,
+    getIconByState: (BiometricsTEIState) -> Int?
 ) {
     val biometricsState = remember(value) {
         if (!value.isNullOrEmpty()) {
             if (value.startsWith(BIOMETRICS_FAILURE_PATTERN)) {
-                BiometricsState.FAILURE
+                BiometricsTEIState.FAILURE
             } else {
-                BiometricsState.SUCCESS
+                BiometricsTEIState.SUCCESS
             }
         } else {
-            BiometricsState.INITIAL
+            BiometricsTEIState.INITIAL
         }
     }
 
-    val biometricsText = remember(biometricsState) {
-        when (biometricsState) {
-            BiometricsState.INITIAL -> R.string.biometrics_get
-            BiometricsState.SUCCESS -> R.string.biometrics_completed
-            BiometricsState.FAILURE -> R.string.biometrics_declined
-        }
-    }
-
-    val biometricsIcon = remember(biometricsState) {
-        when (biometricsState) {
-            BiometricsState.INITIAL -> resources.context.getBioIconNew()
-            BiometricsState.SUCCESS -> resources.context.getBioIconSuccess()
-            BiometricsState.FAILURE -> resources.context.getBioIconWarning()
-        }
-    }
-
-    val biometricsColor = remember(biometricsState) {
-        when (biometricsState) {
-            BiometricsState.INITIAL -> "#4d4d4d"
-            BiometricsState.SUCCESS -> "#FF35835D"
-            BiometricsState.FAILURE -> "#C57704"
-        }
-    }
-
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-    ) {
-        Button(
-            text = stringResource(biometricsText).uppercase(),
-            style = ButtonStyle.TEXT_LIGHT,
-            icon = {
-                Icon(
-                    painter = painterResource(biometricsIcon),
-                    contentDescription = stringResource(biometricsText),
-                    tint = Color.Unspecified,
-                )
-            },
-            modifier = Modifier.background(Color(android.graphics.Color.parseColor(biometricsColor)))
-                .weight(1f)
-                .height(50.dp),
-            onClick = {
-                if (biometricsState == BiometricsState.INITIAL) {
-                    onBiometricsClick()
-                }
-            },
-        )
-
-        if (biometricsState != BiometricsState.INITIAL) {
-            Spacer(modifier = Modifier.width(16.dp))
-
-            Button(
-                text = stringResource(R.string.biometrics_retake).uppercase(),
-                style = ButtonStyle.TEXT_LIGHT,
-                icon = {
-                    Icon(
-                        painter = painterResource(R.drawable.ic_bio_retry),
-                        contentDescription = stringResource(R.string.biometrics_retake),
-                        tint = Color.Unspecified,
-                    )
-                },
-                modifier = Modifier.background(
-                    Color(
-                        android.graphics.Color.parseColor(
-                            "#4d4d4d"
-                        )
-                    )
-                )
-                    .width(200.dp)
-                    .height(50.dp),
-                onClick = { onBiometricsClick() },
-
-                )
-        }
-
-
-    }
+    RegistrationButton(
+        biometricsState=  biometricsState,
+        enabled = true,
+        onBiometricsClick = onBiometricsClick,
+        getIconByState = getIconByState)
 }
--- form/src/main/java/org/dhis2/form/ui/provider/inputfield/BiometricsDataElementProvider.kt
+++ form/src/main/java/org/dhis2/form/ui/provider/inputfield/BiometricsDataElementProvider.kt
@@ -1,10 +1,13 @@
 package org.dhis2.form.ui.provider.inputfield
 
 import androidx.compose.runtime.Composable
+import org.dhis2.commons.biometrics.getBioIconSuccess
+import org.dhis2.commons.biometrics.getBioIconWarning
 import org.dhis2.commons.resources.ResourceManager
 import org.dhis2.form.model.biometrics.BiometricsDataElementUiModelImpl
 import org.dhis2.form.ui.biometrics.components.BiometricsRegistration
 import org.dhis2.form.ui.biometrics.components.BiometricsVerification
+import org.dhis2.form.ui.biometrics.components.biometricsTEIRegistration.BiometricsTEIState
 
 @Composable
 internal fun ProvideBiometricsDataElement(
@@ -14,6 +17,12 @@ internal fun ProvideBiometricsDataElement(
     if (!fieldUiModel.value.isNullOrEmpty()) {
         BiometricsVerification(fieldUiModel.status, fieldUiModel::onRetryVerificationClick)
     } else {
-        BiometricsRegistration(fieldUiModel.value, fieldUiModel::onBiometricsClick, resources)
+        BiometricsRegistration(fieldUiModel.value, fieldUiModel::onBiometricsClick) { state ->
+            when (state) {
+                BiometricsTEIState.INITIAL -> null
+                BiometricsTEIState.SUCCESS -> resources.context.getBioIconSuccess()
+                BiometricsTEIState.FAILURE -> resources.context.getBioIconWarning()
+            }
+        }
     }
 }
