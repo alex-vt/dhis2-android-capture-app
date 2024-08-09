608a4f2eedfc5697babd845fddb904dad3182e7e
xurxodev <xurxosf@gmail.com>
2024-08-09 08:29:56 +0200

    Remove icon of search with biometrics next action

--- app/src/main/java/org/dhis2/usescases/biometrics/ui/SequentialNextSearchAction.kt
+++ app/src/main/java/org/dhis2/usescases/biometrics/ui/SequentialNextSearchAction.kt
@@ -3,12 +3,8 @@ package org.dhis2.usescases.biometrics.ui
 import android.graphics.Color.parseColor
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
@@ -17,7 +13,6 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import org.dhis2.R
@@ -38,15 +33,7 @@ internal fun SequentialNextSearchAction(
                     color = Color(0xFF0281cb)
                 ),
                 onClick = onClick) {
-                Row() {
-                    Icon(
-                        painter = painterResource(org.dhis2.form.R.drawable.ic_bio_fingerprint),
-                        contentDescription = stringResource(org.dhis2.form.R.string.biometrics_get),
-                        tint = Color(0xFF0281cb),
-                    )
-                    Spacer(modifier = Modifier.size(ButtonDefaults.IconSize))
-                    Text(stringResource(R.string.biometrics_search), color = Color(0xFF0281cb))
-                }
+                Text(stringResource(R.string.biometrics_search), color = Color(0xFF0281cb))
             }
         }
         is SequentialSearchAction.SearchWithAttributes -> {
